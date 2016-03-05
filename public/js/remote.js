71c43c688c37d559ef7a8ea526e6201de3f09f07
Anagani Sai Kiran <saikirananagani7@gmail.com>
2016-03-05 23:32:12 +0530

    UPDATED YOUTUBE API to V3
    API KEY BASED USAGE

--- public/js/remote.js
+++ public/js/remote.js
@@ -1,75 +1,66 @@
 var host = document.location.origin;
-var socket = io.connect(host); 
+var socket = io.connect(host);
 socket.on('connect', function(data){
 	socket.emit('remote');
-	
-	//Youtube
-	  var Youtube = {
-		  	getVideo: function(query, socket){
-			  	var max_videos = 12;
-			  	var url = "http://gdata.youtube.com/feeds/api/videos?vq=" + escape(query) + "&max-results=" + max_videos + "&alt=json-in-script&callback=?";
-			  	
-			  	$.getJSON(url, function(data){
-				  	$("ul.video").html("");
-				  	var jsonObj = [];
-			  		$(data.feed.entry).each(function(key, item){
-				  		var a = item.id.$t.split("/"),
-				  			id = a[6],
-				  			title = item.title.$t,
-				  			thumbnail = item.media$group.media$thumbnail[0].url,
-				  			totalSec = item.media$group.yt$duration.seconds,
-				  			hours = parseInt( totalSec / 3600 ) % 24,
-				  			minutes = parseInt( totalSec / 60 ) % 60,
-				  			seconds = totalSec % 60;
-				  		
-				  		var duration = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
-				  		
-				  		
-				  		jsonObj = {
-					  		id:id,
-					  		title:title,
-					  		thumbnail:thumbnail,
-					  		duration:duration};
-					  		
-				  		
-				  		var template = $('#videoTpl').html(),
-				  			html = Mustache.to_html(template, jsonObj);
-				  		$('ul.video').append(html);	
-				  			
-			  		});
-			  		$(".watch").on("click",function(){
-  			  		var video_id = $(this).data('id');
-					socket.emit('video',{action:"play", video_id:video_id});
-			  		});
-			  	});
-			  		
-		  	}
-	  }		
-	
-$$(".r-container").swipeLeft(function(){
-	socket.emit('controll',{action:"swipeLeft"}); 
-});
 
-$$(".r-container").swipeRight(function(){
-	socket.emit('controll',{action:"swipeRight"}); 
-});
-$$(".r-header").tap(function(){
-	socket.emit('controll',{action:"tap"}); 
-	$(".app-body").fadeToggle("fast", function () {});	
-	$.get(host+'/omx/quit',function(data){
-		console.log(data);
+	//Youtube
+	var Youtube = {
+		getVideo: function(query, socket){
+			var apiKey = "{YOUR API KEY HERE}"
+			var max_videos = 25;
+			var url = "https://www.googleapis.com/youtube/v3/search?order=viewcount&part=snippet&q=" + escape(query) + "&type=video+&videoDefinition=high&key=" + apiKey + "&maxResults="+ max_videos;
+
+			$.getJSON(url, function(json){
+				$("ul.video").html("");
+				var jsonObj = [];
+				$(json.items).each(function(key, item){
+					console.log(item.id.videoId+" "+item.snippet.title+" "+item.snippet.thumbnails.default.url);
+					jsonObj = {
+						id: item.id.videoId,
+						title: item.snippet.title,
+						thumbnail: item.snippet.thumbnails.medium.url,
+						description: item.snippet.description
+					};
+
+
+						var template = $('#videoTpl').html(),
+						html = Mustache.to_html(template, jsonObj);
+						$('ul.video').append(html);
+
+					});
+					$(".watch").on("click",function(){
+						var video_id = $(this).data('id');
+						socket.emit('video',{action:"play", video_id:video_id});
+					});
+				});
+
+			}
+		}
+
+		$$(".r-container").swipeLeft(function(){
+			socket.emit('controll',{action:"swipeLeft"});
+		});
+
+		$$(".r-container").swipeRight(function(){
+			socket.emit('controll',{action:"swipeRight"});
+		});
+		$$(".r-header").tap(function(){
+			socket.emit('controll',{action:"tap"});
+			$(".app-body").fadeToggle("fast", function () {});
+			$.get(host+'/omx/quit',function(data){
+				console.log(data);
+			});
+		});
+		$$(".app-body").tap(function(){
+			$.get(host+'/omx/pause',function(data){
+				console.log(data);
+			});
+		});
+		$(".search input").change(function() {
+			Youtube.getVideo($(this).val(), socket);
+		});
+
+		socket.on("loading", function(data){
+			console.log(data);
+		})
 	});
-});
-$$(".app-body").tap(function(){
-	$.get(host+'/omx/pause',function(data){
-		console.log(data);
-	});
-});
-$(".search input").change(function() {
-	Youtube.getVideo($(this).val(), socket);
- });
- 
- socket.on("loading", function(data){
-	 console.log(data);
- })	  
-});
